832cc2e9569f997549058a3ebf3b83479d983511
Jan Gehring <krimdomu@googlemail.com>
2012-02-15 14:22:26 +0100

    don't use Net::Server::Daemonize, use own fork

--- lib/Moirai.pm
+++ lib/Moirai.pm
@@ -24,6 +24,30 @@ use vars qw(@EXPORT);
 use vars qw(@services $log_file);
 
 my %incidents = ();
+my $opts      = {}; 
+
+READ_OPTS: {
+   for(my $i=0; $i<@ARGV; $i++) {
+
+      if($ARGV[$i] =~ m/^\-\-([a-z0-9\-_]+)/) {
+         my $key = $1; 
+         if(! $ARGV[$i+1] || $ARGV[$i+1] =~ m/^\-\-/) {
+            $opts->{$key} = 1;                                                                                      
+         }   
+         else {
+            if(exists $opts->{$key}) {
+               $opts->{$key} = [ $opts->{$key} ] if (! ref $opts->{$key});
+
+               push(@{$opts->{$key}}, $ARGV[++$i]);
+            }   
+            else {
+               $opts->{$key} = $ARGV[++$i];
+            }   
+         }   
+      }   
+
+   }
+}
 
 sub Service {
    my (%service) = @_;
@@ -53,8 +77,33 @@ sub say {
 }
 
 sub run {
-   for my $service (@services) {
-      $service->run;
+   if(exists $opts->{stop}) {
+      for my $service (@services) {
+         $service->stop(%{ $opts });
+      }
+
+      exit 0;
+   }
+
+   # default pid path
+   $opts->{pid_path} ||= "/var/lib";
+
+   # fork starting process
+   my $pid = fork;
+   unless($pid) {
+
+      # close STD* handles
+      close(STDOUT);
+      close(STDERR);
+
+      for my $service (@services) {
+         $service->run(%{ $opts });
+      }
+
+      exit 0;
+   }
+   else {
+      waitpid $pid, 0;
    }
 }
 
--- lib/Moirai/Service.pm
+++ lib/Moirai/Service.pm
@@ -10,6 +10,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use POSIX ":sys_wait_h";
 
 sub new {
    my $that = shift;
@@ -18,13 +19,52 @@ sub new {
 
    bless($self, $proto);
 
+   # default user and group
+   $self->{user}  ||= "daemon";
+   $self->{group} ||= "daemon";
+
+   unless($self->{name}) {
+      die("You have to specify a name for your service.");
+   }
+
    return $self;
 }
 
 sub run {
-   my ($self) = @_;
+   my ($self, %option) = @_;
 
-   $self->_monitor->run;
+   my $pid = fork;
+   unless($pid) {
+      # write pid file for worker
+      my $pid_file_name = $self->{name};
+      $pid_file_name =~ s/[\s\/\\]/_/gms;
+
+      open(my $fh, ">", $option{pid_path} . "/$pid_file_name.pid") or die($!);
+      print $fh $$;
+      close($fh);
+      $self->_monitor->run;
+      exit 0;
+   }
+   elsif($pid) {
+      waitpid ($pid, &WNOHANG);
+   }
+}
+
+sub stop {
+   my ($self, %option) = @_;
+
+   my $pid_file_name = $self->{name};
+   $pid_file_name =~ s/[\s\/\\]/_/gms;
+
+   my $pid_path = $option{pid_path} || "/var/lib";
+   my $pid = eval { local(@ARGV, $/) = ("$pid_path/$pid_file_name.pid"); <>; };
+   chomp $pid;
+
+   if(kill 9, $pid) {
+      unlink("$pid_path/$pid_file_name.pid");
+      return 1;
+   }
+
 }
 
 sub _monitor {
